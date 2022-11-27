790c36f7a96c56f66c4672c2d4bb2ce6566eba03
Mats <49249061+Matseslats@users.noreply.github.com>
2022-11-27 23:17:58 +0000

    Added content option

---
+++ app.js
@@ -0,0 +1,7 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
