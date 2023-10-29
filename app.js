1ff0dbe94019cb2307d42696dafe362b63dbd67a
Mats <49249061+Matseslats@users.noreply.github.com>
2023-10-29 14:50:06 +0000

    Started work on website

--- app.js
+++
@@ -1,7 +0,0 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry) => {
-        entry.target.classList.toggle('show', entry.isIntersecting);
-    });
-});
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
